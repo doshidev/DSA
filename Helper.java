bb20fb035d6a958433bca1c0e7105af4c211b284
Devang Doshi <doshidev@gmail.com>
2018-03-11 12:05:13 -0400

    Insertion Sort

--- Helper.java
+++ Helper.java
@@ -1,7 +1,24 @@
-package PACKAGE_NAME;
-
-/**
- * Created by ddoshi on 3/9/18.
- */
 public class Helper {
+    public static void printArray(int[] intArray) {
+        System.out.print("[");
+        for(int i = 0; i < intArray.length; i++){
+            if(i == 0) {
+                System.out.print(intArray[i]);
+            } else {
+                System.out.print("\t" + intArray[i]);
+            }
+
+        }
+        System.out.println("]");
+    }
+
+
+    public static boolean matchArray(int[] a, int[] b) {
+        if(a.length != b.length) return false;
+        if(a.length <= 0 && b.length <= 0) return true;
+        for (int i = 0; i < a.length; i++) {
+            if(a[i] != b[i]) return false;
+        }
+        return true;
+    }
 }
--- ISort.java
+++ ISort.java
@@ -1,7 +1,31 @@
-package PACKAGE_NAME;
-
-/**
- * Created by ddoshi on 3/11/18.
- */
 public class ISort {
+    public static void main(String[] args) {
+        test(new int[] {14, 11, 1, 3, 8, 5, 13}, new int[] {1, 3, 5, 8, 11, 13, 14});
+        test(new int[] {13}, new int[] {13});
+        test(new int[] {}, new int[] {});
+        test(new int[] {1, 2, 3, 4, 5, 6}, new int[] {1, 2, 3, 4, 5, 6});
+        test(new int[] {10, 9 , 8, 7, 6, 5}, new int[] {5, 6, 7, 8, 9, 10});
+    }
+
+    public static void test(int[] source, int[] expectedResult) {
+        Helper.printArray(source);
+        sortAscending(source);
+        Helper.printArray(source);
+        System.out.println(Helper.matchArray(source, expectedResult));
+        System.out.println();
+    }
+
+    public static void sortAscending(int[] source) {
+        if(source.length <= 1) return;
+        for (int i = 1; i < source.length; i++) {
+            int key = source[i];
+            int j = i;
+            while (j > 0 && key < source[j - 1]) {
+                source[j] = source[j - 1];
+                j--;
+            }
+            source[j] = key;
+        }
+    }
+
 }
